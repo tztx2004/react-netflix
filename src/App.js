55f232d2d836c2ba685e5ae69ca0b5d9ddd3e628
tztx2004 <tztx2004@gmail.com>
2023-08-03 11:50:12 +0900

    메인 부분 일부

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 
 import React from 'react'
 import Nav from './components/Nav';
+import Banner from './components/Banner';
 
 export default function App() {
   return (
     <div className='App'>
-      <Nav/>
+      <Nav />
+      <Banner />
     </div>
   )
 }
