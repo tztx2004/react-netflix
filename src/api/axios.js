7389cea68569851c56cdbc2264ad1e8f2b04cc71
tztx2004 <tztx2004@gmail.com>
2023-08-05 02:32:58 +0900

    updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
     params: {
-        api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
+        api_key: "a4fccb7954afe7cf0f568e139df41b43",
         language: "ko-KR"
     }
 
