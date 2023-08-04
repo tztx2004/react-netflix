26115653c6d3b20b1053bc6b6cd9f67a34d03b4e
tztx2004 <tztx2004@gmail.com>
2023-08-05 02:24:13 +0900

    swiper 적용

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,12 +1,21 @@
 import axios from "../api/axios";
 import React, { useEffect, useState } from "react";
-import './Row.css'
+import "./Row.css";
 import MovieModal from "./MovieModal";
+
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import {Swiper,SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 export default function Row({ title, id, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
-  const [modalOpen, setModalOpen] = useState(false)
-  const [movieSelected, setMovieSelected] = useState({})
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({});
 
   useEffect(() => {
     fetchMovieData();
@@ -14,48 +23,61 @@ export default function Row({ title, id, fetchUrl, isLargeRow }) {
   const fetchMovieData = async () => {
     const request = await axios.get(fetchUrl);
     setMovies(request.data.results);
-    
   };
 
-  const handleClick = (movie)=>{
-    setModalOpen(true)
-    setMovieSelected(movie)
-  }
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelected(movie);
+  };
 
   return (
     <section className="row">
       <h2>{title}</h2>
-      <div className="slider">
-        <div className="slider__arrow-left">
-          <span className="arrow" onClick={()=>{
-            document.getElementById(id).scrollLeft -= window.innerWidth -80
-          }}>{"<"}</span>
-        </div>
+      <Swiper 
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        loop={true} // loop 기능을 사용할 것인지
+        breakpoints={{
+          1378: {
+            slidesPerView: 6, // 한번에 보이는 슬라이드 개수
+            slidesPerGroup: 6, // 몇개씩 슬라이드 할지
+          },
+          998: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
+        navigation  // arrow 버튼 사용 유무 
+        pagination={{ clickable: true }} // 페이지 버튼 보이게 할지 
+      >
         <div id={id} className="row__posters">
           {movies.map((movie) => (
-            <img
-              key={movie.id}
-              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-              src={`https://image.tmdb.org/t/p/original/${
-                isLargeRow ? movie.poster_path : movie.backdrop_path
-              } `}
-              alt={movie.name}
-              onClick={()=>handleClick(movie)}
-            />
+            <SwiperSlide>
+              <img
+                key={movie.id}
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`https://image.tmdb.org/t/p/original/${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                } `}
+                alt={movie.name}
+                onClick={() => handleClick(movie)}
+              />
+            </SwiperSlide>
           ))}
         </div>
-        <div className="slider__arrow-right">
-          <span className="arrow" onClick={()=>{
-            document.getElementById(id).scrollLeft += window.innerWidth -80
-          }}>{">"}</span>
-        </div>
-      </div>
+      </Swiper>
 
-        {
-          modalOpen && (
-            <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
-          )
-        }
+      {modalOpen && (
+        <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+      )}
     </section>
   );
 }
